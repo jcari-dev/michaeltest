cb03de9736574bb2fe272711da2f6a4ee1d94346
Jorge Caridad <582119jc@gmail.com>
2021-06-26 13:46:32 -0400

    testing

---
+++ models/favoriteSongSchema.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+
+
+const favoriteSongSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    favoriteSong: String,
+    whyFavorite: String
+}, {timestamps: true});
+
+const favoriteSongCollection = mongoose.model('favsong', favoriteSongSchema, 'favoriteSong');
+
+module.exports = favoriteSongCollection
